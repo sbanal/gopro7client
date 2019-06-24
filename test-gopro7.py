d7300548a25aa57e0b838dab7edd4001ec8204fd
Stephen Lou Banal <stephen.banal@gmail.com>
2019-06-24 13:05:18 +1000

    test burst photo

--- test-gopro7.py
+++ test-gopro7.py
@@ -14,15 +14,16 @@ print('status:', gopro.status())
 # logging.info('take_photo_result: %s', take_photo_result)
 
 ## Multi-shot
-# mode_result = gopro.set_mode(mode=PrimaryMode.MULTI_SHOT, sub_mode=MultiShotSubMode.BURST)
-# logging.info('set photo mode: %s', mode_result)
-# multi_shot_result = gopro.take_photo()
-# logging.info('multi_shot_result: %s', multi_shot_result)
+mode_result = gopro.set_mode(mode=PrimaryMode.MULTI_SHOT, sub_mode=MultiShotSubMode.BURST)
+logging.info('set photo mode: %s', mode_result)
+multi_shot_result = gopro.take_photo()
+logging.info('multi_shot_result: %s', multi_shot_result)
 
 ## Video
-mode_result = gopro.set_mode(mode=PrimaryMode.VIDEO, sub_mode=VideoSubMode.LOOPING)
-logging.info('set video mode: %s', mode_result)
-gopro.start_video()
-time.sleep(5)
-gopro.stop_video()
+# mode_result = gopro.set_mode(mode=PrimaryMode.VIDEO, sub_mode=VideoSubMode.LOOPING)
+# logging.info('set video mode: %s', mode_result)
+# gopro.start_video()
+# time.sleep(5)
+# gopro.stop_video()
+
 
